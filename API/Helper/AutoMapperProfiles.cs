58584e1380a3565b78c12a1c9f8ae4b957cb01f2
Arpit Saini <arpitsaini07@gmail.com>
2024-03-13 17:50:17 +0530

    AddingLoadingFeature

--- API/Helper/AutoMapperProfiles.cs
+++ API/Helper/AutoMapperProfiles.cs
@@ -1,4 +1,5 @@
-﻿using API.Entities;
+﻿using API.DTOs;
+using API.Entities;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc.TagHelpers;
 
@@ -11,5 +12,6 @@ public class AutoMapperProfiles : Profile
            .ForMember(dest=> dest.PhotoUrl,opt=>opt.MapFrom(src=>src.Photos.FirstOrDefault(x=>x.IsMain).Url))
            .ForMember(dest => dest.Age, opt => opt.MapFrom(src=>src.DateOfBirth.CalculateAge()));
         CreateMap<Photo,PhotoDto>();
+        CreateMap<MemberUpdateDto, AppUser>();
     }
 }
